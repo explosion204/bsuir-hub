62cfa610e7a216fd8b2f83ee5f81d604d6a0da08
explosion204 <dzmitriy20magic@gmail.com>
2021-08-15 16:37:56 +0300

    cookies fix

--- src/main/java/com/karnyshov/bsuirhub/controller/filter/CookiesFilter.java
+++ src/main/java/com/karnyshov/bsuirhub/controller/filter/CookiesFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -17,21 +18,32 @@ import static com.karnyshov.bsuirhub.controller.command.RequestParameter.LOCALE_
  * This filter prevents application usage with disabled cookies.
  * If application is not working without cookies in a proper way, it is a good solution to restrict
  * inconvenient user-experience at all.
- * Also, it sets a new locale as a cookie.
+ * Also, it sets a new locale as a cookie if a "localeCode" parameter is passed with request.
  * @author Dmitry Karnyshov
  */
 @WebFilter(filterName = "CookiesFilter")
 public class CookiesFilter implements Filter {
     private static final String LOCALE_COOKIE_NAME = "localeCode";
+    private static final String COOKIE_CHECK = "cookieCheck";
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
 
-        // redirect to error page if cookies are disabled by client
         if (httpRequest.getCookies() == null) {
-            httpRequest.getRequestDispatcher(DISABLED_COOKIES_JSP).forward(request, response);
+            // no cookies, it might be the first request to application, so we have to make a little check
+            if (session.getAttribute(COOKIE_CHECK) != null) {
+                // if it were "check" request to ensure that server is able to set cookies
+                // and cookies are still null (i.e. disabled by client), then forward to error page
+                httpRequest.getRequestDispatcher(DISABLED_COOKIES_JSP).forward(request, response);
+            } else {
+                // if it is not a "check" request and cookies are null, then redirect user to the same location
+                // but with session COOKIE_CHECK flag.
+                session.setAttribute(COOKIE_CHECK, COOKIE_CHECK);
+                httpResponse.sendRedirect(httpRequest.getRequestURI());
+            }
         } else {
             // set new locale
             String localeCode = request.getParameter(LOCALE_CODE);
@@ -42,6 +54,7 @@ public class CookiesFilter implements Filter {
                 httpResponse.addCookie(localeCookie);
             }
 
+            session.removeAttribute(COOKIE_CHECK);
             chain.doFilter(request, response);
         }
     }
