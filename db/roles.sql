2811b760455810eee784864836670cc116c7b3b6
explosion204 <dzmitriy20magic@gmail.com>
2021-08-31 23:05:25 +0300

    included gradle-wrapper.jar

--- db/roles.sql
+++
@@ -1,3 +0,0 @@
-INSERT roles VALUES (1, 'STUDENT');
-INSERT roles VALUES (2, 'TEACHER');
-INSERT roles VALUES (3, 'ADMIN');
--- db/statuses.sql
+++
@@ -1,3 +0,0 @@
-INSERT statuses VALUES (1, 'NOT_CONFIRMED');
-INSERT statuses VALUES (2, 'CONFIRMED');
-INSERT statuses VALUES (3, 'DELETED');
